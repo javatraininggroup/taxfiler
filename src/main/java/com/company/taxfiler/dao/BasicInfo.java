bbfce5ecd80904ec9ad6e8cd8874737d8e98ba5c
manoj <manumanoj782@gmail.com>
2020-07-20 19:11:35 +0530

    basic info

---
+++ src/main/java/com/company/taxfiler/dao/BasicInfo.java
@@ -0,0 +1,21 @@
+package com.company.taxfiler.dao;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "basic_info")
+public class BasicInfo {
+	
+	private boolean martialStatus;
+	private String name;
+	private int ssn;
+	private Date dob;
+	private String occupation;
+	private Date dateOfMarriage;
+	private Date firstDateOfEnrtyInUS;
+	private String typeOfVisa;
+
+}
