94a04bcac753346a77a7e6432a9f3c54de7a74d6
nikhil <nikhilchinchadu@gmail.com>
2020-07-20 18:46:56 +0500

    getters setters added

--- src/main/java/com/company/taxfiler/dao/BankDetails.java
+++ src/main/java/com/company/taxfiler/dao/BankDetails.java
@@ -22,6 +22,42 @@ private String bankAccountTpe;
 private long id;
 @Column(name="tax_filed_year_id")
 private long taxFiledYearId;
+public String getBankName() {
+	return bankName;
+}
+public void setBankName(String bankName) {
+	this.bankName = bankName;
+}
+public long getRoutingNumber() {
+	return routingNumber;
+}
+public void setRoutingNumber(long routingNumber) {
+	this.routingNumber = routingNumber;
+}
+public long getBankAccountNumber() {
+	return bankAccountNumber;
+}
+public void setBankAccountNumber(long bankAccountNumber) {
+	this.bankAccountNumber = bankAccountNumber;
+}
+public String getBankAccountTpe() {
+	return bankAccountTpe;
+}
+public void setBankAccountTpe(String bankAccountTpe) {
+	this.bankAccountTpe = bankAccountTpe;
+}
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public long getTaxFiledYearId() {
+	return taxFiledYearId;
+}
+public void setTaxFiledYearId(long taxFiledYearId) {
+	this.taxFiledYearId = taxFiledYearId;
+}
 
 
 }
