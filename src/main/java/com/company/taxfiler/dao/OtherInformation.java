5b1623c1ccf2eb97e1260f308abffe774d6a3a98
nikhil <nikhilchinchadu@gmail.com>
2020-07-20 20:26:24 +0500

    completed

--- src/main/java/com/company/taxfiler/dao/OtherInformation.java
+++ src/main/java/com/company/taxfiler/dao/OtherInformation.java
@@ -7,8 +7,29 @@ import javax.persistence.Table;
 @Entity	
 @Table(name="other_informtion")
 public class OtherInformation {
+	
+	private long id;
+	
+	@Column(name = "tax_filed_year_id")
+	private long taxFiledYearId;
 
 	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getTaxFiledYearId() {
+		return taxFiledYearId;
+	}
+
+	public void setTaxFiledYearId(long taxFiledYearId) {
+		this.taxFiledYearId = taxFiledYearId;
+	}
+
 	@Column(name="please_update_if_any_other_information")
 	
 	private String pleaseUpdateIfAnyOtherInformation;
