06de65dfe4317e9ae2779f43838e82aee3e86439
javatraininggroup <60401543+javatraininggroup@users.noreply.github.com>
2020-11-22 20:25:15 +0530

    settings model added by Rama

---
+++ src/main/java/com/company/model/SettingsModel.java
@@ -0,0 +1,59 @@
+package com.company.model;
+
+import java.io.Serializable;
+
+public class SettingsModel  implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String newPassword;
+	private String currentPassword;
+	private String confirmPassword;
+	private String name;
+	private String phone;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	public String getCurrentPassword() {
+		return currentPassword;
+	}
+	public void setCurrentPassword(String currentPassword) {
+		this.currentPassword = currentPassword;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+
+
+
+
+}
